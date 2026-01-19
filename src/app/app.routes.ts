54194d212b0a7cddd4a8dfdca29a98d71d7528de
joseleonec <joleonca@gmail.com>
2026-01-18 22:36:31 -0500

    Add CuentaListComponent route and update sidebar link

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,16 @@ import { ClienteCreateComponent } from './components/cliente/cliente-create/clie
 import { MovimientoCreateComponent } from './components/movimientos/movimiento-create/movimiento-create.component';
 import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
 import { CuentaCreateComponent } from './components/cuentas/cuenta-create/cuenta-create.component';
+import { CuentaListComponent } from './components/cuentas/cuenta-list/cuenta-list.component';
 import { ReporteViewComponent } from './components/reportes/reporte-view/reporte-view.component';
 
 export const routes: Routes = [
   { path: 'clientes', component: ClienteListComponent },
   { path: 'clientes/create', component: ClienteCreateComponent },
+  { path: 'cuentas', component: CuentaListComponent },
   { path: 'cuentas/create', component: CuentaCreateComponent },
   { path: 'movimientos/create', component: MovimientoCreateComponent },
+
   { path: 'reportes', component: ReporteViewComponent },
   { path: '', redirectTo: '/clientes', pathMatch: 'full' }
 ];
