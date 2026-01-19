2e1b3d1c3a19fbe098a9639e0cf2d7120aa7f5a8
joseleonec <joleonca@gmail.com>
2026-01-19 00:27:41 -0500

    Refactor cliente.service.ts to remove unused ClienteRequest import

--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { ClienteRequest, ClienteResponse } from '../models/cliente.dto';
 import { environment } from '../../environments/environment';
+import { ClienteResponse } from '../models/cliente.dto';
 
 @Injectable({
   providedIn: 'root'
